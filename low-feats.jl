a0a12106f1d388fd079187135cd12201db6afa60
fundamental <mark.d.mccurry@gmail.com>
2016-04-12 16:48:40 -0400

    Cleanup Low Feature Code

--- low-feats.jl
+++ low-feats.jl
@@ -1,35 +1,23 @@
 #Features used in Philip Low's Dissertation...
 
-function max_frequency(spectra::Matrix{Float64})
+low_cf(i,w,s) = sum(w.*s[:,i][:].^2.0)/sum(s[:,i].^2.0)
+low_hf(i,w,s) = w[indmax(s[:,i])]
+low_lf(i,w,s) = w[indmin(s[:,i])]
+
+
+"""
+Produce 1D Feature representation for Low style features
+
+    These features don't generalize terribly well, but they have helped motivate
+    the work in the rest of this repository
+"""
+function low_feat(spectra::Matrix{Float64}; func=low_cf)
     N       = size(spectra,2)        #Temporal observations
     feat    = zeros(size(spectra,2)) #Output sequence of features
     M       = size(spectra,1)        #Frequency bins
     weights = linspace(0,100,M)      #Range of signal in Hz
     for i=1:N
-        feat[i] = weights[indmax(spectra[:,i])]
-    end
-    feat
-end
-
-
-function central_frequency(spectra::Matrix{Float64}, n::Float64=2.0)
-    N       = size(spectra,2)        #Temporal observations
-    feat    = zeros(size(spectra,2)) #Output sequence of features
-    M       = size(spectra,1)        #Frequency bins
-    weights = linspace(0,100,M)      #Range of signal in Hz
-    for i=1:N
-        feat[i] = sum(weights.*spectra[:,i][:].^n)/sum(spectra[:,i].^n)
-    end
-    feat
-end
-
-function min_frequency(spectra::Matrix{Float64})
-    N       = size(spectra,2)        #Temporal observations
-    feat    = zeros(size(spectra,2)) #Output sequence of features
-    M       = size(spectra,1)        #Frequency bins
-    weights = linspace(0,100,M)      #Range of signal in Hz
-    for i=1:N
-        feat[i] = weights[indmin(spectra[:,i])]
+        feat[i] = func(i,weights,spectra)
     end
     feat
 end
