1366ff711aaf6af420e0fb00b427cb4a821911e7
fundamental <mark.d.mccurry@gmail.com>
2016-10-18 15:16:53 -0400

    Cleanup Gradient Varient Of Zoneify

--- misc.jl
+++ misc.jl
@@ -11,35 +11,53 @@ function obtainGradient(samples::Vector{Float64})
     if(N == 1)
         return samples
     end
-    (a,b) = [linspace(0,1,N) ones(N)]\samples
+    (c,a,b) = [linspace(0,1,N).^2 linspace(0,1,N) ones(N)]\samples
     out = zeros(N)
     for i=1:N
-        out[i] = a*(i-1)/(N-1)+b
+        out[i] = c*((i-1)/(N-1))^2 + a*(i-1)/(N-1)+b
     end
     out
 end
 
-function zoneifyGradient(img, X, Y)
+function zoneifyGradient(img, X, Y; operator=mean)
     I = float(copy(img))
-    xedge = find(X)
+    I[:,:] = 0
+    xedge = X;find(X)
     #println("xedge = ", xedge)
-    yedge = find(Y)
-    prepend!(xedge, [1])
-    prepend!(yedge, [1])
-    push!(yedge, size(img)[1])
-    push!(xedge, size(img)[2])
+    yedge = Y;find(Y)
+    #prepend!(xedge, [1])
+    #prepend!(yedge, [1])
+    #push!(yedge, size(img)[1])
+    #push!(xedge, size(img)[2])
     #println(size(img))
     #println(xedge)
     #println(yedge)
     #println(maximum(yedge))
+
+    #XXX this old code only worked for when Y was densely sampled
+    #for yy=1:length(yedge)-1, xx=1:length(xedge)-1
+    #    I[yedge[yy]:yedge[yy+1],xedge[xx]:xedge[xx+1]] = obtainGradient(img[yedge[yy]:yedge[yy+1],xedge[xx]:xedge[xx+1]][:])
+    #end
     for yy=1:length(yedge)-1, xx=1:length(xedge)-1
-        I[yedge[yy]:yedge[yy+1],xedge[xx]:xedge[xx+1]] = obtainGradient(img[yedge[yy]:yedge[yy+1],xedge[xx]:xedge[xx+1]][:])
+        y1 = yedge[yy]
+        y2 = yedge[yy+1]
+        x1 = xedge[xx]
+        x2 = xedge[xx+1]
+        #println([y1, y2, x1, x2])
+        #println((y2-y1)*(x2-x1))
+        row_mean = operator(img[y1:y2,x1:x2],2)
+        row_grad = obtainGradient(row_mean[:])
+        col_mean = operator(img[y1:y2,x1:x2],1)
+        col_grad = obtainGradient(col_mean[:])
+        for y=y1:y2, x=x1:x2
+            I[y,x] = (row_grad[1+y-y1]+col_grad[1+x-x1])/2
+        end
     end
     I
 end
 
 function viewStuff(SubjectID::String, workDir::String, amount::Float64,
-    misc=false,alt_work=nothing)
+    misc=false,alt_work=nothing;x_thresh=0,y_thresh=-1)
     ep  = readcsv("$workDir/edgeParameter$SubjectID.csv")
     dat = nothing
     if(alt_work != nothing)
@@ -61,14 +79,32 @@ function viewStuff(SubjectID::String, workDir::String, amount::Float64,
     end
     ll  = readcsv("$workDir/Dejunkedlabels$SubjectID.csv")
 
-    X = find(maximum(ep,2)[:])
-    Y = find(ones(size(dat)[1]))
+    X = nothing
+    Y = nothing
+    X = find(maximum(ep,2)[:].>x_thresh)
+    if(y_thresh == -1)
+        Y = find(ones(size(dat)[1]))
+    else
+        coverhigh = PyPlot.imread("$workDir/coverhigh$SubjectID.png")
+        bandshigh = identifyInterestBands(map(Float64, coverhigh[1:1000,:]))
+        covermed = PyPlot.imread("$workDir/covermed$SubjectID.png")
+        bandsmed = identifyInterestBands(map(Float64, covermed[1:1000,:]))
+        coverlow = PyPlot.imread("$workDir/coverlow$SubjectID.png")
+        bandslow = identifyInterestBands(map(Float64, coverlow[1:1000,:]))
+        Y     = find((bandshigh[1:end-1] .!= bandshigh[2:end]) |
+                     (bandsmed[1:end-1]  .!= bandsmed[2:end])  |
+                     (bandslow[1:end-1]  .!= bandslow[2:end]))
+        prepend!(Y, [1])
+        push!(Y, 1000)
+    end
+    println("X.length = ", length(X)*100/size(dat,2),"%")
+    println("Y.length = ", length(Y)*100/size(dat,1),"%")
     prepend!(X,[1])
     push!(X,size(dat,2))
 
     if(misc)
-        mn = zoneifyGradient(dat, X, Y)
-        md = zoneify(dat, X, Y, operator=median)
+        mn = zoneifyGradient(dat, X, Y, operator=mean)
+        md = zoneifyGradient(dat, X, Y, operator=median)
     else
         mn = zoneify(dat, X, Y, operator=mean)
         md = zoneify(dat, X, Y, operator=median)
